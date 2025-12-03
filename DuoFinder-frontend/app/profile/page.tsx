fdb542c5ccdada99b49f48771b2a5b905595cb75
Nahue <97365665+PainSword@users.noreply.github.com>
2025-12-03 19:52:12 -0300

    FE - Cambios de imagenes
    En la landing page, en el log in, register y un icono nuevo en mi perfil.

--- DuoFinder-frontend/app/profile/page.tsx
+++ DuoFinder-frontend/app/profile/page.tsx
@@ -43,7 +43,7 @@ const PRESET_AVATARS = [
   },
   {
     id: 'controller',
-    url: 'https://images.unsplash.com/photo-1592841200221-a6898f307baa?auto=format&fit=crop&w=800&q=80',
+    url: 'https://images.unsplash.com/photo-1619382581049-c87bedd3b479?q=80&w=735&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
   },
   {
     id: 'setup',
